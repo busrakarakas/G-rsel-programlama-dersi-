73ecd1e455d6a601206548368f261a4917373006
Büşra <karakasbusrasu@gmail.com>
2025-11-16 19:44:58 +0300

    Create 5.ünite_sonucevapları5.16.cs

---
+++ 5.ünite_sonucevapları5.16.cs
@@ -0,0 +1,20 @@
+ using System;
+ 
+  class Mystery
+  {
+    static void Main()
+    {
+       int number = 1;
+       int sum1 = 0, sum2 = 0;
+ 
+       while (number <= 15)
+       {
+          if (number % 2 != 0)
+              sum1 += number;
+          else
+              sum2 += number;
+          ++number;
+       Console.WriteLine($"{sum1} \t {sum2}");
+      }  
+   }
+  }
