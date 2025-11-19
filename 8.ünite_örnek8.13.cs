e49ad940b55fa22f59358ebed61f72bf429b919e
Büşra <karakasbusrasu@gmail.com>
2025-11-19 19:56:00 +0300

    Create 8.ünite_örnek8.13.cs

---
+++ 8.ünite_örnek8.13.cs
@@ -0,0 +1,50 @@
+using System;
+
+class PassArray
+{
+   // Main creates array and calls ModifyArray and ModifyElement
+   static void Main()
+   {
+      int[] array = {1, 2, 3, 4, 5};
+
+      Console.WriteLine("Effects of passing reference to entire array:");
+      Console.WriteLine("The values of the original array are:");
+
+      // output original array elements 
+      foreach (var value in array)
+      {
+         Console.Write($"   {value}");
+      }
+
+      ModifyArray(array); // pass array reference
+      Console.WriteLine("\n\nThe values of the modified array are:");
+
+      // output modified array elements 
+      foreach (var value in array)
+      {
+         Console.Write($"   {value}");
+      }
+
+      Console.WriteLine("\n\nEffects of passing array element value:\n" +
+         $"array[3] before ModifyElement: {array[3]}");
+
+      ModifyElement(array[3]); // attempt to modify array[3]
+      Console.WriteLine($"array[3] after ModifyElement: {array[3]}");
+   }
+
+   // multiply each element of an array by 2                     
+   static void ModifyArray(int[] array2)
+   {
+      for (var counter = 0; counter < array2.Length; ++counter)
+      {
+         array2[counter] *= 2;
+      }
+   }
+
+   // multiply argument by 2                                 
+   static void ModifyElement(int element)
+   {
+      element *= 2;
+      Console.WriteLine($"Value of element in ModifyElement: {element}");
+   }
+}
